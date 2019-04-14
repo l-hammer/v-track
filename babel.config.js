40e30ceb2472ead3558d10df9afeb156df090d54
宋慧武 <songhuiwu001@ke.com>
2019-04-14 16:19:28 +0800

    :fire:uninstall useless @babel plugins

--- babel.config.js
+++ babel.config.js
@@ -7,13 +7,7 @@ module.exports = {
       }
     ]
   ],
-  plugins: [
-    // "@babel/plugin-transform-async-to-generator",
-    // "@babel/plugin-external-helpers",
-    // "@babel/plugin-transform-runtime",
-    // "@babel/plugin-transform-modules-commonjs",
-    "@babel/plugin-proposal-class-properties"
-  ],
+  plugins: ["@babel/plugin-proposal-class-properties"],
   env: {
     test: {
       presets: ["@babel/preset-env"]
