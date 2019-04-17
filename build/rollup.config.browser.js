e572e4cb3c8666c70b2b5f7af2d436f2d4434b03
宋慧武 <songhuiwu001@ke.com>
2019-04-17 12:08:13 +0800

    :wrench:add replace plugin for build:browser

--- build/rollup.config.browser.js
+++ build/rollup.config.browser.js
@@ -1,14 +1,21 @@
 import base from "./rollup.config.base";
 import { uglify } from "rollup-plugin-uglify";
+import replace from "rollup-plugin-replace";
 
 const config = Object.assign({}, base, {
   output: {
-    name: "v-track",
+    name: "VTrack",
     file: "dist/v-track.min.js",
     format: "umd"
   }
 });
 
-config.plugins.push(uglify({}));
+config.plugins = [
+  ...config.plugins,
+  uglify({}),
+  replace({
+    "process.env.NODE_ENV": JSON.stringify("production")
+  })
+];
 
 export default config;
