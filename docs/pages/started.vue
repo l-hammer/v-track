48c48e97310c7e4ec5e48c6fb77644e8e814bc70
宋慧武 <songhuiwu001@ke.com>
2019-07-30 21:55:36 +0800

    :sparkles:uvpv support routeUpdate mode

--- docs/pages/started.vue
+++ docs/pages/started.vue
@@ -59,7 +59,7 @@ import trackEvents from "./track-events";
 Vue.use(VTrack, {
   trackEvents, // 埋点事件对象
   trackEnable: {
-    UVPV: true, // 是否开启UVPV统计，默认为false
+    UVPV: true, // 是否开启UVPV统计，v0.8.3新增routeUpdate，即在当前路由参数发生改变时埋点，默认为false
     TONP: true // 是否开启页面停留时长统计，默认为false
   }
 })
